bb79e208ea384904874f40fef0027ab7ced5aab5
Yassin Moussaoui <yassinmoussaouibusiness@gmail.com>
2024-05-09 09:23:53 -0600

    added c# lsp

--- lua/yacheen/plugins/telescope.lua
+++ lua/yacheen/plugins/telescope.lua
@@ -4,8 +4,9 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         -- i hate u windows
-        -- { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        -- { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        -- macos
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
         "nvim-tree/nvim-web-devicons",
     },
     config = function()
