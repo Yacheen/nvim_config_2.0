036dff61c1acda33d72d9db119074fc101defc0d
Yassin <yassinmoussaouibusiness@gmail.com>
2025-07-26 16:42:10 -0600

    Update mason.lua

--- lua/yacheen/plugins/lsp/mason.lua
+++ lua/yacheen/plugins/lsp/mason.lua
@@ -25,20 +25,9 @@ return {
             -- list of servers for mason to install
             ensure_installed = {
                 "rust_analyzer",
-                "taplo", -- toml files
-                "ts_ls",
-                "html",
-                "cssls",
-                "tailwindcss",
                 "lua_ls",
                 -- "pyright"
-                "graphql",
-                "emmet_ls", -- idk
                 "bashls",
-                "dockerls",
-                "marksman", -- markdown
-                "clangd",
-                "csharp_ls"
             },
             -- auto-install configured servers (with lspconfig)
             automatic_installation = true, -- not the same as ensure_installed
